f7e764b82b79b413aaa32effef3f5d5db095151f
vishalini.v <90790080+vishalini25@users.noreply.github.com>
2023-03-30 19:34:02 +0530

    Create style.css

---
+++ TASK-2/CALCULATOR/style.css
@@ -0,0 +1,56 @@
+.calculator {
+	margin: 0 auto;
+	padding: 20px;
+	border: 1px solid #ccc;
+	border-radius: 5px;
+	width: 280px;
+  }
+  
+  .display {
+	margin-bottom: 10px;
+  }
+  
+  .display input[type="text"] {
+	width: 100%;
+	height: 40px;
+	border-radius: 5px;
+	padding: 5px;
+	font-size: 20px;
+	text-align: right;
+  }
+  
+  .buttons button {
+	width: 50px;
+	height: 50px;
+	font-size: 20px;
+	border-radius: 5px;
+	margin: 5px;
+	cursor: pointer;
+  }
+  
+  .operator {
+	background-color: #ff9a00;
+	color: #fff;
+  }
+  
+  .buttons button:nth-child(14) {
+	background-color: #ff9a00;
+	color: #fff;
+  }
+  
+  .buttons button:nth-child(15) {
+	background-color: #007aff;
+	color: #fff;
+  }
+  
+  .buttons button:last-child {
+	margin-right: 0;
+  }
+  
+  .buttons button:focus {
+	outline: none;
+  }
+  
+  .buttons button:active {
+	background-color: #ccc;
+  }
